a3b6e7333e4ea7071dc65e11a27b1f333a5b8afd
Jérémy MOREAU <jmoreau.dev@gmail.com>
2016-02-09 07:59:23 +0100

    Fix layout

--- core/src/com/frappagames/pendu/Screens/DifficultyScreen.java
+++ core/src/com/frappagames/pendu/Screens/DifficultyScreen.java
@@ -1,9 +1,9 @@
 package com.frappagames.pendu.Screens;
 
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.frappagames.pendu.LePendu;
@@ -12,58 +12,52 @@ import com.frappagames.pendu.LePendu;
  * Created by jmoreau on 11/01/16.
  */
 public class DifficultyScreen extends GameScreen {
-    private TextureRegion title;
-    private TextureRegion decos;
-    private TextureRegion easyTexture;
-    private TextureRegion normalTexture;
-    private TextureRegion hardTexture;
     private ImageButton easyBtn;
     private ImageButton normalBtn;
     private ImageButton hardBtn;
+    private Image titleImg;
+    private Image decosImg;
 
     public DifficultyScreen(final LePendu game) {
         super(game);
 
-        title         = game.atlas.findRegion("title");
-        decos         = game.atlas.findRegion("decos");
-        easyTexture   = game.atlas.findRegion("easyBtn");
-        normalTexture = game.atlas.findRegion("normalBtn");
-        hardTexture   = game.atlas.findRegion("hardBtn");
+        titleImg  = new Image(new TextureRegionDrawable(game.atlas.findRegion("title")));
+        decosImg  = new Image(new TextureRegionDrawable(game.atlas.findRegion("decos")));
+        easyBtn   = new ImageButton(new TextureRegionDrawable(game.atlas.findRegion("easyBtn")));
+        normalBtn = new ImageButton(new TextureRegionDrawable(game.atlas.findRegion("normalBtn")));
+        hardBtn   = new ImageButton(new TextureRegionDrawable(game.atlas.findRegion("hardBtn")));
 
-        easyBtn       = new ImageButton(new TextureRegionDrawable(easyTexture));
+        VerticalGroup vg = new VerticalGroup();
+        vg.addActor(easyBtn);
+        vg.addActor(normalBtn);
+        vg.addActor(hardBtn);
+
+        table.add(titleImg).top().padTop(169);
+        table.row();
+        table.add(vg).expandY();
+        table.row();
+        table.add(decosImg).bottom().padBottom(50);
+
         easyBtn.addListener(new ChangeListener() {
             public void changed(ChangeListener.ChangeEvent event, Actor actor) {
                 game.setScreen(new PlayScreen(game, 1));
             }
         });
-
-        normalBtn     = new ImageButton(new TextureRegionDrawable(normalTexture));
         normalBtn.addListener(new ChangeListener() {
             public void changed(ChangeListener.ChangeEvent event, Actor actor) {
                 game.setScreen(new PlayScreen(game, 2));
             }
         });
-
-        hardBtn       = new ImageButton(new TextureRegionDrawable(hardTexture));
         hardBtn.addListener(new ChangeListener() {
             public void changed(ChangeListener.ChangeEvent event, Actor actor) {
                 game.setScreen(new PlayScreen(game, 3));
             }
         });
-
-        table.add(easyBtn).row();
-        table.add(normalBtn).row();
-        table.add(hardBtn).row();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-
-        game.batch.begin();
-        game.batch.draw(title, 95, 1030);
-        game.batch.draw(decos, 45, 80);
-        game.batch.end();
     }
 
     @Override
--- core/src/com/frappagames/pendu/Screens/MenuScreen.java
+++ core/src/com/frappagames/pendu/Screens/MenuScreen.java
@@ -1,7 +1,7 @@
 package com.frappagames.pendu.Screens;
 
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
@@ -11,36 +11,33 @@ import com.frappagames.pendu.LePendu;
  * Created by jmoreau on 11/01/16.
  */
 public class MenuScreen extends GameScreen {
-    private TextureRegion title;
-    private TextureRegion decos;
-    private TextureRegion playTexture;
     private ImageButton playBtn;
+    private Image titleImg;
+    private Image decosImg;
 
     public MenuScreen(final LePendu game) {
         super(game);
 
-        title       = game.atlas.findRegion("title");
-        playTexture = game.atlas.findRegion("playBtn");
-        decos       = game.atlas.findRegion("decos");
+        titleImg = new Image(new TextureRegionDrawable(game.atlas.findRegion("title")));
+        playBtn  = new ImageButton(new TextureRegionDrawable(game.atlas.findRegion("playBtn")));
+        decosImg = new Image(new TextureRegionDrawable(game.atlas.findRegion("decos")));
 
-        playBtn     = new ImageButton(new TextureRegionDrawable(playTexture));
+        table.add(titleImg).top().padTop(169);
+        table.row();
+        table.add(playBtn).expandY();
+        table.row();
+        table.add(decosImg).bottom().padBottom(50);
+
         playBtn.addListener(new ChangeListener() {
             public void changed(ChangeListener.ChangeEvent event, Actor actor) {
                 game.setScreen(new DifficultyScreen(game));
             }
         });
-
-        table.add(playBtn);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-
-        game.batch.begin();
-        game.batch.draw(title, 95, 1030);
-        game.batch.draw(decos, 45, 80);
-        game.batch.end();
     }
 
     @Override
